1e4f1f58ac6ef101aaa7626db69307a27d5a165d
mnadev <mohammednadeem902@gmail.com>
2020-11-07 22:47:57 -0500

    created file to store constants

---
+++ app/constants.py
@@ -0,0 +1,30 @@
+PRAYER_CALCULATION_JURISTIC_METHOD_STRINGS = ["Jafari - Ithna Ashari",
+                                              "Karachi - University of Islamic Sciences",
+                                              "ISNA - Islamic Society of North America",
+                                              "MWL - Muslim World League",
+                                              "Mecca - Umm al-Qura",
+                                              "Egyptian General Authority of Survey",
+                                              "Custom Setting",
+                                              "University of Tehran - Institute of Geophysics",
+                                              "Algerian Minister of Religious Affairs and Wakfs",
+                                              "Gulf 90 Minutes Fixed Isha",
+                                              "Egyptian General Authority of Survey (Bis)",
+                                              "UOIF - Union Des Organisations Islamiques De France",
+                                              "Sistem Informasi Hisab Rukyat Indonesia",
+                                              "Diyanet İşleri Başkanlığı",
+                                              "Germany Custom",
+                                              "Russia Custom"]
+
+ASR_JURISTIC_METHOD_STRINGS = ["Standard (Shafi, Hanbli, Maliki)", "Hanafi"]
+
+HIGH_LATITUDES_STRING = ["None", "Midnight", "1/7th", "Angle"]
+
+TIME_FORMAT_STRINGS = ["24 Hour Format", "12 Hour Format", "12 Hour Format w/o Suffix", "Floating Number"]
+
+MAGHRIB_RULE_STRINGS = ["Angle", "Fixed Minutes"]
+
+ISHA_RULE_STRINGS = ["Angle", "Fixed Minutes"]
+
+PRAYER_TIMES_URL = "http://www.islamicfinder.us/index.php/api/"
+
+HIJRI_DATE_URL = "http://api.aladhan.com/v1/gToH"
