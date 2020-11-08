c8900b02afb3a446bad8adbfd2ce9e33b00246d9
mnadev <mohammednadeem902@gmail.com>
2020-11-07 21:13:56 -0500

    updated background color and text color in app

--- app/main.py
+++ app/main.py
@@ -4,6 +4,8 @@ kivy.require('2.0.0')
 from kivy.app import App
 from kivy.uix.label import Label
 from main_screen import MainScreen
+from kivy.core.window import Window
+Window.clearcolor = (1, 1, 1, 1)
 
 class MuezzinApp(App):
   def build(self):
