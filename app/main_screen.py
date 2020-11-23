af4d0083b157e4b20dc711601461765348a19b24
mnadev <mohammednadeem902@gmail.com>
2020-11-22 20:40:36 -0500

    added pane for prayer times

--- app/main_screen.py
+++ app/main_screen.py
@@ -9,8 +9,15 @@ class MainScreen(GridLayout):
     self.add_widget(Label(text=datetime.datetime.now().strftime("%I:%M") , color=[0, 0, 0, 1]))
     self.add_widget(Label(text="Saturday, October 20, 2020", color=[0, 0, 0, 1]))
     self.add_widget(Label(text="14 Ramadan 1440 AH", color=[0, 0, 0, 1]))
+    self.add_widget(PrayerPane())
+
+class PrayerPane(GridLayout):
+  def __init__(self, **kwargs):
+    super(PrayerPane, self).__init__(**kwargs)
+    self.cols = 1
+    self.rows = 6
     self.add_widget(Label(text="Fajr: 5:04 AM", color=[0, 0, 0, 1]))
-    self.add_widget(Label(text="Sunrise: 5:04 AM", color=[0, 0, 0, 1]))
+    self.add_widget(Label(text="Duha: 5:04 AM", color=[0, 0, 0, 1]))
     self.add_widget(Label(text="Dhuhr: 5:04 AM", color=[0, 0, 0, 1]))
     self.add_widget(Label(text="Asr: 5:04 AM", color=[0, 0, 0, 1]))
     self.add_widget(Label(text="Maghrib: 5:04 AM", color=[0, 0, 0, 1]))
