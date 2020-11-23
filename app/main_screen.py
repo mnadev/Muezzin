f3892d91a1e694186444edb9e8df87c5f5154097
mnadev <mohammednadeem902@gmail.com>
2020-11-22 22:23:12 -0500

    changed layout to move date on top and clock on bottom

--- app/main_screen.py
+++ app/main_screen.py
@@ -7,15 +7,23 @@ import datetime
 class MainScreen(GridLayout):
   def __init__(self, **kwargs):
     super(MainScreen, self).__init__(**kwargs)
-    self.cols = 3
+    self.cols = 2
 
+    self.add_widget(TimePane())
+    self.add_widget(PrayerPane(size_hint=(0.2, 1)))
+
+
+class TimePane(GridLayout):
+  def __init__(self, **kwargs):
+    super(TimePane, self).__init__(**kwargs)
+    self.cols = 1
+    self.rows = 2
     self.time_widget = Label(text=datetime.datetime.now().strftime("%I:%M %p"), color=[1, 1, 1, 1],
                              font_name="RobotoMono-Regular",
-                             size_hint=(0.4, 1), font_size="30sp")
+                             size_hint=(1, 0.5), font_size="30sp")
 
-    self.add_widget(CalendarPane(size_hint=(0.4, 1.0)))
+    self.add_widget(CalendarBox(size_hint=(1, 0.5)))
     self.add_widget(self.time_widget)
-    self.add_widget(PrayerPane(size_hint=(0.2, 1)))
 
     Clock.schedule_once(self.update, datetime.datetime.now().second % 60)
 
@@ -24,17 +32,17 @@ class MainScreen(GridLayout):
     Clock.schedule_once(self.update, datetime.datetime.now().second % 60)
 
 
-class CalendarPane(GridLayout):
+class CalendarBox(GridLayout):
   def __init__(self, **kwargs):
-    super(CalendarPane, self).__init__(**kwargs)
-    self.cols = 1
-    self.rows = 2
+    super(CalendarBox, self).__init__(**kwargs)
+    self.cols = 2
+    self.rows = 1
     self.add_widget(
       WrappedLabel(text="Saturday, October 20, 2020", color=[0, 0, 0, 1], font_name="Roboto-Bold", font_size="16sp",
-                   size_hint=(1, 0.25)))
+                   size_hint=(0.5, 1), padding=("10sp", "0sp")))
     self.add_widget(
       WrappedLabel(text="14 Ramadan 1440 AH", color=[0, 0, 0, 1], font_name="Roboto-Bold", font_size="16sp",
-                   size_hint=(1, 0.25)))
+                   size_hint=(0.5, 1), padding=("10sp", "0sp")))
 
 
 class PrayerPane(GridLayout):
