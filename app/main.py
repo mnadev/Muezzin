e1183c394d6e9d6dea70f40f7c0c045179d1a3fb
mnadev <mohammednadeem902@gmail.com>
2020-11-22 20:45:47 -0500

    set defined size for app window based on 3.5inch pi touch screen size

--- app/main.py
+++ app/main.py
@@ -1,10 +1,13 @@
 import kivy
 kivy.require('2.0.0')
 
+from kivy.config import Config
+Config.set('graphics', 'width', '480')
+Config.set('graphics', 'height', '320')
+
 from kivy.app import App
-from kivy.uix.label import Label
-from main_screen import MainScreen
 from kivy.core.window import Window
+from main_screen import MainScreen
 
 Window.clearcolor = (1, 1, 1, 1)
 
