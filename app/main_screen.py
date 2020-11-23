eca57b14bd4bf0fdce99be5fe1cbecbf7ffb4de4
mnadev <mohammednadeem902@gmail.com>
2020-11-22 20:33:00 -0500

    clock bootstraps with current time

--- app/main_screen.py
+++ app/main_screen.py
@@ -1,11 +1,12 @@
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.label import Label
+import datetime
 
 class MainScreen(GridLayout):
   def __init__(self, **kwargs):
     super(MainScreen, self).__init__(**kwargs)
     self.cols = 5
-    self.add_widget(Label(text="8:49 PM", color=[0, 0, 0, 1]))
+    self.add_widget(Label(text=datetime.datetime.now().strftime("%I:%M") , color=[0, 0, 0, 1]))
     self.add_widget(Label(text="Saturday, October 20, 2020", color=[0, 0, 0, 1]))
     self.add_widget(Label(text="14 Ramadan 1440 AH", color=[0, 0, 0, 1]))
     self.add_widget(Label(text="Fajr: 5:04 AM", color=[0, 0, 0, 1]))
