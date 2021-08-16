349293fd19278ef010514ea5d9c67e5d41d0005a
mnadev <mohammednadeem902@gmail.com>
2021-08-16 19:22:09 -0400

    auto formatted audio player and alarm popup

--- app/alarm_popup.py
+++ app/alarm_popup.py
@@ -2,42 +2,43 @@ from kivymd.uix.button import MDRaisedButton
 from kivymd.uix.dialog import MDDialog
 from kivymd.uix.gridlayout import MDGridLayout
 
+
 class AlarmDismissPopup:
-  """
-  Creates a MDDialog to dismiss the alarm and holds logic to show dialog, dismiss dialog and shut off alarm.
-  """
-
-  def __init__(self, audio_player):
     """
-    Creates an AlarmDismissPopup
+    Creates a MDDialog to dismiss the alarm and holds logic to show dialog, dismiss dialog and shut off alarm.
     """
-    self.popup_layout = MDGridLayout(cols=1, padding=10)
 
-    self.dismiss_button = MDRaisedButton(text="Dismiss")
-    self.dismiss_button.bind(on_press=self.dismiss)
+    def __init__(self, audio_player):
+        """
+        Creates an AlarmDismissPopup
+        """
+        self.popup_layout = MDGridLayout(cols=1, padding=10)
 
-    self.alarm_popup = MDDialog(title='Dismiss Alarm',
-                                size_hint=(0.75, 0.75),
-                                buttons=[self.dismiss_button])
-    self.alarm_popup.on_touch_up(self.dismiss)
-    self.is_open = False
+        self.dismiss_button = MDRaisedButton(text="Dismiss")
+        self.dismiss_button.bind(on_press=self.dismiss)
 
-    self.audio_player = audio_player
+        self.alarm_popup = MDDialog(
+            title="Dismiss Alarm", size_hint=(0.75, 0.75), buttons=[self.dismiss_button]
+        )
+        self.alarm_popup.on_touch_up(self.dismiss)
+        self.is_open = False
 
-  def dismiss(self, *args):
-    """
-    Dismisses the alarm popup and silences the alarm.
-    :param args: Args sent by Kivy
-    :return: None
-    """
-    self.alarm_popup.dismiss()
-    self.is_open = False
-    self.audio_player.stop_alarm()
+        self.audio_player = audio_player
 
-  def open(self):
-    """
-    Opens the alarm dismissal popup
-    :return: None
-    """
-    self.alarm_popup.open()
-    self.is_open = True
+    def dismiss(self, *args):
+        """
+        Dismisses the alarm popup and silences the alarm.
+        :param args: Args sent by Kivy
+        :return: None
+        """
+        self.alarm_popup.dismiss()
+        self.is_open = False
+        self.audio_player.stop_alarm()
+
+    def open(self):
+        """
+        Opens the alarm dismissal popup
+        :return: None
+        """
+        self.alarm_popup.open()
+        self.is_open = True
--- app/audio_player.py
+++ app/audio_player.py
@@ -1,20 +1,20 @@
-
 from kivy.clock import Clock
 from kivy.core.audio import SoundLoader
 
+
 class AudioPlayer:
     def __init__(self):
-        self.adhan = SoundLoader.load('res/adhan.mp3')
+        self.adhan = SoundLoader.load("res/adhan.mp3")
         self.adhan.seek(0)
 
-        self.fajr_adhan = SoundLoader.load('res/fajr_adhan.mp3')
+        self.fajr_adhan = SoundLoader.load("res/fajr_adhan.mp3")
         self.fajr_adhan.seek(0)
 
-        self.alarm = SoundLoader.load('res/alarm.mp3')
+        self.alarm = SoundLoader.load("res/alarm.mp3")
         self.alarm.seek(0)
 
         self.reschedule_alarm_event = None
-    
+
     def play_adhan(self):
         self.adhan.play()
 
@@ -42,4 +42,3 @@ class AudioPlayer:
         self.alarm.seek(0)
         self.alarm.play()
         self.reschedule_alarm_event = Clock.schedule_once(self.reset_alarm, 17)
-
