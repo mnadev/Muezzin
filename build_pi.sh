95f51ae4c3b64d2c49fe76d8bd599bec8471ac03
Mohammad Nadeem <mohammednadeem902@gmail.com>
2021-08-21 12:03:40 -0400

    fixed issues in build script for Pi

--- build_pi.sh
+++ build_pi.sh
@@ -1,16 +1,18 @@
 #!/usr/bin/env bash
-echo "Building App"
-pyinstaller -y --clean --windowed muezzin.spec > /dev/null
-echo "Packaging"
-pushd dist > /dev/null
-tar czf ../../muezzin.tar muezzin/ > /dev/null
-popd > /dev/null
-cd ..
-echo "Extracting"
-mkdir muezzin_app
-mv muezzin.tar muezzin_app
-cd muezzin_app
-tar -xvf muezzin.tar > /dev/null
-echo "Package succesfully built"
+echo "Building App";
+pyinstaller -y --clean --windowed muezzin.spec > /dev/null;
+echo "Packaging";
+cd dist > /dev/null;
+touch ../../muezzin.tar;
+chmod 644 ../../muezzin.tar;
+tar czf ../../muezzin.tar muezzin/ > /dev/null;
+cd .. > /dev/null;
+cd ..;
+echo "Extracting";
+mkdir muezzin_app;
+mv muezzin.tar muezzin_app;
+cd muezzin_app;
+tar -xvf muezzin.tar > /dev/null;
+echo "Package succesfully built";
 
 
